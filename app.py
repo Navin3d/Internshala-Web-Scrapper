05e87ccbb0dd471603ef9f206631586aee73bc37
Navin3d <smnavin65@mail.com>
2023-02-02 19:05:27 +0530

    Added Regex for Passed Out Year

--- app.py
+++ app.py
@@ -5,9 +5,11 @@ import re
 
 
 cookies = {
+    
 }
 
 headers = {
+
 }
 
 params = {
@@ -15,6 +17,9 @@ params = {
 }
 
 response = requests.get('https://internshala.com/student/resume', params=params, cookies=cookies, headers=headers)
+
+
+data = dict()
 
 
 def get_github_username(text):
@@ -25,29 +30,40 @@ def get_github_username(text):
     github_links = ""
 
     for link in links:
-        git_link_matched = re.findall("https://github.com/([A-Za-z0-9]{1,})+/?$", str(link.get("href")))
+        exact_link = str(link.get("href"))
+        git_link_matched = re.findall("https://github.com/([A-Za-z0-9]{1,})+/?$", exact_link)
         if git_link_matched:
             github_links = git_link_matched
+            data["github_url"] = exact_link
 
-    print(github_links)
+    data["github_username"] = github_links[0]
 
-    return github_links[0]
+
+def get_passout_year(text):
+    years = [1, 0, 10, 2]
+    soup = BeautifulSoup(text, features="html.parser")
+    divs = soup.find(education_type="college")
+    for div in divs:
+        match = re.findall(r'([2][0-9]{3})', div.getText())
+        if len(match) > 0:
+            for year in match:
+                years.append(int(year))
+
+    years.sort()
+    data["passout_year"] = years[len(years) - 1]
 
 
 def get_github_stats(username):
-    stats = dict()
     github_page = urlopen(f"https://github-readme-stats.vercel.app/api?username={username}")
     commit_soup = BeautifulSoup(github_page.read(), features="html.parser")
     texts = commit_soup.find_all("text")
 
     for text in texts:
         if text.get("data-testid") == "commits":
-            stats["commits"] = int(text.get_text())
-
-    print(f'Total commits in (2023): {stats["commits"]}')
-    return stats
+            data["github_commits"] = int(text.get_text())
 
 
-github_username = get_github_username(response.content)
-result = get_github_stats(github_username)
-print(result)
+get_passout_year(response.content)
+get_github_username(response.content)
+get_github_stats(data["github_username"])
+print(data)
